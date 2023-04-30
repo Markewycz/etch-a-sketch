6b1aebce552efce2d69df21e1d56aaf10dd4676a
Markewycz <gracjanmarkiewicz00@gmail.com>
2023-05-01 00:28:59 +0200

    Add grid layout option

--- script.js
+++ script.js
@@ -2,14 +2,19 @@ const container = document.querySelector(".sketch-container");
 const div = document.createElement("div");
 const btnChangeGrid = document.querySelector(".change-grid");
 let gridCellsPrompt = 16;
+let sketch;
+
+// FORMULA 960 / gridCellsPrompt = cell width/height px
 
 // Generate grid
 const calcGrid = () => gridCellsPrompt * gridCellsPrompt;
-let sketch;
+const calcCell = () => 960 / gridCellsPrompt;
 
 const generateGrid = () => {
   for (let i = 0; i < calcGrid(); i++) {
     const div = document.createElement("div");
+    div.style.width = calcCell() + "px";
+    div.style.height = calcCell() + "px";
     container.appendChild(div).classList.add("sketch-cell");
   }
   sketch = document.querySelectorAll(".sketch-cell");
@@ -36,7 +41,7 @@ sketchMode();
 // Change grid cells per row
 btnChangeGrid.addEventListener("click", () => {
   gridCellsPrompt = +prompt("How many cells per row you want?");
-  console.log(gridCellsPrompt);
+  if (gridCellsPrompt >= 100) return alert("Pick number up to 100!");
   clearGrid();
   generateGrid();
   sketchMode();
