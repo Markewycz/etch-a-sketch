0acc370b530c2dcf5c6c4bf2225d38f13ab9314d
Markewycz <gracjanmarkiewicz00@gmail.com>
2023-05-01 21:06:19 +0200

    Add rainbow brush / Clean code

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 const container = document.querySelector(".sketch-container");
 const div = document.createElement("div");
 const btnChangeGrid = document.querySelector(".change-grid");
+const black = document.querySelector("#radio-one");
+const rainbow = document.querySelector("#radio-two");
 let gridCellsPrompt = 16;
 let sketch;
 
-// FORMULA 960 / gridCellsPrompt = cell width/height px
-
-// Generate grid
 const calcGrid = () => gridCellsPrompt * gridCellsPrompt;
-const calcCell = () => 960 / gridCellsPrompt;
+const calcCell = () => 600 / gridCellsPrompt;
+const randomColor = () => {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
 
 const generateGrid = () => {
   for (let i = 0; i < calcGrid(); i++) {
@@ -18,8 +25,20 @@ const generateGrid = () => {
     container.appendChild(div).classList.add("sketch-cell");
   }
   sketch = document.querySelectorAll(".sketch-cell");
+
+  sketch.forEach((cell) => {
+    cell.addEventListener("mouseover", (e) => {
+      if (e.buttons === 1) {
+        if (black.checked) cell.style.backgroundColor = "black";
+        if (rainbow.checked) cell.style.backgroundColor = randomColor();
+      }
+    });
+  });
 };
 generateGrid();
+
+///////////////////////////////////
+// FORMULA canvas width / gridCellsPrompt = cell width/height px
 
 // Clear existing grid
 const clearGrid = () => {
@@ -28,21 +47,10 @@ const clearGrid = () => {
   });
 };
 
-// Sketching method
-const sketchMode = () => {
-  sketch.forEach((cell) => {
-    cell.addEventListener("mouseover", (e) => {
-      if (e.buttons === 1) cell.classList.add("painted");
-    });
-  });
-};
-sketchMode();
-
 // Change grid cells per row
 btnChangeGrid.addEventListener("click", () => {
   gridCellsPrompt = +prompt("How many cells per row you want?");
   if (gridCellsPrompt >= 100) return alert("Pick number up to 100!");
   clearGrid();
   generateGrid();
-  sketchMode();
 });
