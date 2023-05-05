d5e5facfc5ff08ef58bc1ec762a5fa441948cc45
Markewycz <gracjanmarkiewicz00@gmail.com>
2023-05-05 23:15:19 +0200

    Add style, modify composition

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-const container = document.querySelector(".sketch-container");
-const div = document.createElement("div");
+const container = document.querySelector(".board-sketch");
 
 const sliderGrid = document.querySelector("#change-grid");
 const gridPreview = document.querySelector(".grid-preview");
@@ -8,16 +7,19 @@ const btnClearGrid = document.querySelector(".clear-grid");
 const btnEraser = document.querySelector(".eraser");
 const colorPicker = document.querySelector(".color-picker");
 const rainbow = document.querySelector(".rainbow");
+const shadow = document.querySelector(".shadow");
 const eraser = document.querySelector(".eraser");
 
 let gridCellsPrompt = 16;
 let sketch;
 let mode = 0;
 
+15625;
+125;
 ///////////////////
 // Functions
 const calcGrid = () => gridCellsPrompt * gridCellsPrompt;
-const calcCell = () => 600 / gridCellsPrompt;
+const calcCell = () => 500 / gridCellsPrompt;
 const randomColor = () => {
   const letters = "0123456789ABCDEF";
   let color = "#";
@@ -36,7 +38,7 @@ const generateGrid = () => {
   }
   sketch = document.querySelectorAll(".sketch-cell");
 
-  gridPreview.textContent = `${sliderGrid.value}x${sliderGrid.value}`
+  gridPreview.textContent = `${sliderGrid.value}x${sliderGrid.value}`;
 
   sketch.forEach((cell) => {
     cell.addEventListener("mouseover", (e) => {
@@ -44,7 +46,27 @@ const generateGrid = () => {
         if (mode === 0) cell.style.backgroundColor = colorPicker.value;
         if (mode === 1) cell.style.backgroundColor = randomColor();
         if (mode === 2) cell.style.backgroundColor = "";
+        if (mode === 3) {
+          let opacity = 0.1;
+
+          if (!cell.style.backgroundColor) {
+            cell.style.backgroundColor = `rgba(0, 0, 0, ${opacity})`;
+          }
+
+          console.log(cell.style.backgroundColor);
+          if (cell.style.backgroundColor === `rgba(0, 0, 0, ${opacity})`) {
+            opacity += 0.1;
+            cell.style.backgroundColor = `rgba(0, 0, 0, ${opacity})`;
+            console.log(cell.style.backgroundColor, opacity);
+          }
+        }
       }
+      cell.addEventListener("click", (e) => {
+        if (mode === 0) cell.style.backgroundColor = colorPicker.value;
+        if (mode === 1) cell.style.backgroundColor = randomColor();
+        if (mode === 2) cell.style.backgroundColor = "";
+        if (mode === 3) console.log(cell);
+      });
     });
   });
 };
@@ -62,22 +84,16 @@ const clearGrid = () => {
 colorPicker.addEventListener("click", () => (mode = 0));
 rainbow.addEventListener("click", () => (mode = 1));
 eraser.addEventListener("click", () => (mode = 2));
+shadow.addEventListener("click", () => (mode = 3));
 
 btnClearGrid.addEventListener("click", () => {
   clearGrid();
   generateGrid();
 });
 
+const previewGrid = (value) => (gridPreview.textContent = `${value}x${value}`);
 const changeGrid = (value) => {
   gridCellsPrompt = value;
-  gridPreview.textContent = `${value}x${value}`;
   clearGrid();
   generateGrid();
 };
-
-// sliderGrid.addEventListener("click", () => {
-//   gridCellsPrompt = +prompt("How many cells per row you want?");
-//   if (gridCellsPrompt >= 100) return alert("Pick number up to 100!");
-//   clearGrid();
-//   generateGrid();
-// });
